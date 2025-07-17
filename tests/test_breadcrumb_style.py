a76983a9bed145ebb3455151f72d00c23467337d
5h1gerun <sasaki6y.uki@icloud.com>
2025-07-17 12:20:13 +0900

    \xE3\x82\B9\xE3\x83\9E\xE3\x83\9B\xE7\x89\88\xE3\x81\AE\xE3\x83\91\xE3\x83\B3\xE3\x81\8F\xE3\x81\9A\xE3\x83\AA\xE3\x82\B9\xE3\x83\88\xE3\x82\92\xE5\x88\86\xE9\x9B\xA2\xE3\x83\87\xE3\x82\B6\xE3\x82\A4\xE3\x83\B3\xE3\x81\AB

--- tests/test_breadcrumb_style.py
+++ tests/test_breadcrumb_style.py
@@ -1,13 +1,18 @@
 from pathlib import Path
 
 CSS = Path(__file__).resolve().parents[1] / 'web' / 'static' / 'css' / 'style-fresh.css'
-TEMPLATES = [
+PHONE_CSS = Path(__file__).resolve().parents[1] / 'web' / 'static' / 'css' / 'style-phone.css'
+
+PC_TEMPLATES = [
     Path(__file__).resolve().parents[1] / 'web' / 'templates' / 'index.html',
     Path(__file__).resolve().parents[1] / 'web' / 'templates' / 'partials' / 'home.html',
     Path(__file__).resolve().parents[1] / 'web' / 'templates' / 'shared' / 'folder_view.html',
     Path(__file__).resolve().parents[1] / 'web' / 'templates' / 'shared' / 'index.html',
-    Path(__file__).resolve().parents[1] / 'web' / 'templates' / 'mobile' / 'folder_view.html',
     Path(__file__).resolve().parents[1] / 'web' / 'templates' / 'gdrive_import.html',
+]
+
+PHONE_TEMPLATES = [
+    Path(__file__).resolve().parents[1] / 'web' / 'templates' / 'mobile' / 'folder_view.html',
     Path(__file__).resolve().parents[1] / 'web' / 'templates' / 'mobile' / 'gdrive_import.html',
 ]
 
@@ -16,7 +21,16 @@ def test_css_has_breadcrumb_pills():
     assert '.breadcrumb-pills' in text
     assert '\\F285' in text
 
-def test_templates_use_breadcrumb_pills():
-    for tpl in TEMPLATES:
+
+def test_phone_css_has_breadcrumb_phone():
+    text = PHONE_CSS.read_text(encoding='utf-8')
+    assert '.breadcrumb-phone' in text
+    assert 'content: ">"' in text
+
+def test_templates_use_correct_breadcrumb_classes():
+    for tpl in PC_TEMPLATES:
         html = tpl.read_text(encoding='utf-8')
         assert 'breadcrumb-pills' in html
+    for tpl in PHONE_TEMPLATES:
+        html = tpl.read_text(encoding='utf-8')
+        assert 'breadcrumb-phone' in html
